1f56517d1e3c17c9bfa10102111b26c710531565
turosgolyo <papp.dani2206@gmail.com>
2025-10-10 10:21:12 +0200

    params pizza

--- params/src/pages/Param.tsx
+++ params/src/pages/Param.tsx
@@ -1,12 +1,25 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import type { Pizza } from "../types/Pizza";
+import apiClient, { BACKEND_URL } from "../api/apiClient";
 
 const Param = () => {
-  const { kategoria, nev } = useParams();
+  const { id } = useParams();
+  const [pizza, setPizzak] = useState<Pizza>();
+
+  useEffect(() => {
+    apiClient
+      .get(`/pizzak/${Number(id)}`)
+      .then((response) => setPizzak(response.data))
+      .catch((reason) => alert(reason));
+  }, []);
 
   return (
     <>
-      <h1>Kategoria: {kategoria}</h1>
-      <h2>Nev: {nev}</h2>
+      <div>
+        <h1>{pizza?.nev}</h1>
+        <img src={`${BACKEND_URL}/kepek/${pizza?.imageUrl}`} width={300} />
+      </div>
     </>
   );
 };
--- pizzak/src/Pizza.tsx
+++
@@ -1,9 +0,0 @@
-import { useParams } from 'react-router-dom';
-import './App.css';
-import apiClient from './api/apiClient';
-import type { Pizza } from './types/Pizza';
-
-function Pizza() {
-    return <></>;
-}
-export default Pizza;
