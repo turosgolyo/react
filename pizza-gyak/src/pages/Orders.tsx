d3cc3ce686926ecd018913905a7227d86d58a8ee
turosgolyo <papp.dani2206@gmail.com>
2026-01-09 08:44:41 +0100

    orders

--- pizza-gyak/src/pages/Orders.tsx
+++ pizza-gyak/src/pages/Orders.tsx
@@ -1,35 +1,55 @@
 import { useState, useEffect } from "react";
-import { Container, Row } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
+import { Container, Row, Table } from "react-bootstrap";
 import apiClient from "../api/apiClient";
 import type { Order } from "../types/Order";
+import type { Pizza } from "../types/Pizza";
 
 const Orders = () => {
   const [orders, setOrders] = useState<Array<Order>>([]);
+  const [pizzak, setPizzak] = useState<Array<Pizza>>([]);
 
   useEffect(() => {
     apiClient
       .get("/rendelesek")
       .then((response) => setOrders(response.data))
       .catch((err) => {
-        toast.error("vmi nem jo");
+        console.error(err);
+      });
+
+    apiClient
+      .get("/pizzak")
+      .then((response) => setPizzak(response.data))
+      .catch((err) => {
         console.error(err);
       });
   }, []);
 
-  return (
-    <>
-      <Container>
-        <Row>
-          {orders.map((i) => (
-            <h1>
-              {i.pizzaId} - {i.mennyiseg}
-            </h1>
-          ))}
-        </Row>
-      </Container>
-    </>
+  return localStorage.getItem("credentials") ? (
+    <Container>
+      <Table>
+        <thead>
+          <th>Pizza</th>
+          <th>Mennyiseg</th>
+          <th>Ar</th>
+        </thead>
+        <tbody>
+          {orders.map((i) => {
+            const pizza = pizzak.find((p) => p.id == i.pizzaId);
+            return (
+              <tr>
+                <td>{pizza?.nev}</td>
+                <td>{i.mennyiseg} db</td>
+                <td>{(pizza?.ar) * (i.mennyiseg)} Ft</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+    </Container>
+  ) : (
+    <Container>
+      <h1>Nem vagy bejelentkezve</h1>
+    </Container>
   );
 };
 
